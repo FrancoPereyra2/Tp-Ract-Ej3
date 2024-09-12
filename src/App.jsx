3eb0d91c4b15c1d6701b473a455ebd6e57c90028
Franco Pereyra <francopereyra325@gmail.com>
2024-09-11 23:28:19 -0300

    ejercicio 3 terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [msj, setMsj] = useState("Hello my friend!");
 
+  const cambioClick = () => {
+    setMsj("Hello My friend (from changed state)!");
+  };
+  
   return (
     <section>
-      
+      <h1>{msj}</h1>
+      <button onClick={cambioClick}>Click me</button>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
